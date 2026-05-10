af49f4b13ea574f4d948f59a674b4b16fc688197
Firalen <140722327+Firalen@users.noreply.github.com>
2026-05-10 13:06:16 +0300

    Export components from index.ts in system-a

---
+++ apps/system-a/index.ts
@@ -0,0 +1,3 @@
+export { FlashcardDeck, type Flashcard } from "./FlashcardDeck";
+export { StudyMetricsPanel } from "./StudyMetricsPanel";
+export { ReadinessBoard, type ReadinessTopic } from "./ReadinessBoard";
